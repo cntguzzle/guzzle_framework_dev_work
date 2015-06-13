647feb9a68f0e039a786f99be97f03d7292b7524
x <x@x.local>
2015-06-13 13:13:19 -0700

    php to create gist from guzzle
    Signed-off-by: x <x@x.local>

---
+++ ___guzzle_github_POST_GIST_CREATE_GIST_WORKING.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Request;
+
+function get_time(){
+    return time();
+};
+
+$github_api_token = '295398f3dc4da7da1eb717babf948179a91a0860';
+
+$github_gist_paramaters = array(
+    'description' => ''.get_time(),
+    'public' => true,
+    'files' => array('file_name.txt' => array('content' => 'data inside the file'))
+);
+
+
+$json_data = json_encode($github_gist_paramaters);
+
+
+$client = new Client();
+
+$request = $client->createRequest('POST', 'https://api.github.com/gists',[
+    'headers' => [
+    'User-Agent' => 'GUZZLE_DEMO',
+        'Content-Type' => 'application/json'
+        ],
+    'auth' => ['token','295398f3dc4da7da1eb717babf948179a91a0860'],
+    
+    'body' => $json_data
+    ]
+);
+
+$response = $client->send($request);
+var_dump($response->getBody()->getContents());
+
+?>
